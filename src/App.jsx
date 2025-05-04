5179d616b08860aa9db55e0dcfb925f69cdccf46
Shivam <kumarshivam3788@gmail.com>
2025-05-05 02:11:28 +0530

    Add main layout components and routing setup with CSS styles
    - Implemented Header and Footer components for layout structure.
    - Created MainLayout component to wrap the application with Header, Outlet, and Footer.
    - Set up React Router with routes for Home, FetchOld, FetchRQ, FetchIndv, and InfiniteScroll pages.
    - Added CSS styles for body, typography, buttons, and layout elements.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { FetchOld } from './pages/FetchOld';
+import { FetchRQ } from './pages/FetchRQ';
+import { FetchIndv } from './pages/FetchIndv';
+import { InfiniteScroll } from './pages/InfiniteScroll';
+import { MainLayout } from './components/layout/MainLayout';
+import "./App.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/trad",
+        element: <FetchOld />,
+      },
+      {
+        path: "/rq",
+        element: <FetchRQ />,
+      },
+      {
+        path: "/rq/:id",
+        element: <FetchIndv />,
+      },
+      {
+        path: "/infinite",
+        element: <InfiniteScroll />,
+      },
+    ],
+  },
+]);
 
 export const App = () => {
   return (
-    <div>Tanstack Query</div>
+    <RouterProvider router={router}/>
   )
 }
 
