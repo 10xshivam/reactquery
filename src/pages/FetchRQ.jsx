a6e3ae7c4fb9c5726c3aaa156c78f62de01487d3
Shivam <kumarshivam3788@gmail.com>
2025-05-05 03:08:47 +0530

    Refactor fetchPosts to handle async API call and update FetchRQ component to display posts using React Query

--- src/pages/FetchRQ.jsx
+++ src/pages/FetchRQ.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import { fetchPosts } from "../API/api";
+import { useQuery } from "@tanstack/react-query";
 
 export const FetchRQ = () => {
+  const { data } = useQuery({
+    queryKey:['posts'],
+    queryFn:fetchPosts
+  })
+
   return (
-    <div>FetchRQ</div>
-  )
-}
+    <div>
+      <ul className="section-accordion">
+        {data?.map((curElem) => {
+          const { id, title, body } = curElem;
+          return (
+            <li key={id}>
+              <p>{title}</p>
+              <p>{body}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
